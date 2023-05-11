01ee13fcfc27eb868d149f404b876454440d2347
dudonz <edgars@dudins.lv>
2023-05-11 16:53:09 +0300

    Do some styles for the nav bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,10 +14,10 @@
     </head>
     <body class="antialiased">
         <div class="relative">
-            <nav>
-                <a href="{{route('tasks.index')}}">To Do</a>
-                <a href="{{route('tasks.create')}}">Create</a>
-                <a href="{{route('tasks.archived')}}">Archived</a>
+            <nav class="p-2">
+                <a class="border-1" href="{{route('tasks.index')}}">To Do</a>
+                <a class="border-1" href="{{route('tasks.create')}}">Create</a>
+                <a class="border-1" href="{{route('tasks.archived')}}">Archived</a>
             </nav>
         </div>
         <div class="bold-ghost-white relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
