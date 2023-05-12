f500717f10e4168e42517458ee4f323f6314acd0
dudonz <edgars@dudins.lv>
2023-05-12 11:32:40 +0300

    Do some clean up to speed up the app

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,6 @@
 
         <title>@yield('title')</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-
         <link rel="stylesheet" href="{{url('css/styles.css')}}">
     </head>
     <body class="antialiased">
